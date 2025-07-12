e11918990e9c3d1b97bde349417659bd72755837
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-13 00:26:17 +0530

    Added folder

--- client/videoCalling/src/main.jsx
+++ client/videoCalling/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import { StrictMode } from 'react'
+import {} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import reportWebVitals from './reportWebVitals'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
