8590b27718d8d12db66dbc05cd5283d1e8dfb0b7
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-13 23:58:48 +0530

    Added changes

--- client/videoCalling/src/screens/Lobby.jsx
+++ client/videoCalling/src/screens/Lobby.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 const Lobby = () => {
+  const [email,setEmail] = useState("");
+  const [room,setRoom] = useState("");
+
+  const handleSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log({
+      email,
+      room,
+    });
+  })
+
   return (
     <div>
         <h1>Lobby</h1>
-        <form>
-          <label>Email ID</label>
-          <input type="email" />
+        <form onSubmit={handleSubmitForm}>
+          <label htmlFor='email'>Email ID</label>
+          <input type="email" id='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <br/>
+          <label htmlFor='room'>Room Number</label>
+          <input type="text" id='room' value={room} onChange={(e) => setRoom(e.target.value)} />
+          <br />
+          <button>Join</button>
         </form>
     </div>
   )
