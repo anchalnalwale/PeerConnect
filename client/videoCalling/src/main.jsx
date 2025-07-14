f5fe44d1d53a8602010911df934ddff8606884b1
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-14 19:58:21 +0530

    Adding socket code

--- client/videoCalling/src/main.jsx
+++ client/videoCalling/src/main.jsx
@@ -5,12 +5,14 @@ import { StrictMode } from 'react'
 import {BrowserRouter} from 'react-router'
 import './index.css'
 import App from './App.jsx'
-// import reportWebVitals from './reportWebVitals'
+import { SocketProvider } from './context/SocketProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App/>
+      <SocketProvider>
+        <App/>
+      </SocketProvider>
     </BrowserRouter>
   </StrictMode>,
 );
