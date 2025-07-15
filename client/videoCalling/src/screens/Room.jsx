e28df3dbc69c23f447ea37b78886fb60b08c7ccc
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-15 20:59:09 +0530

    Adding functionalities

--- client/videoCalling/src/screens/Room.jsx
+++ client/videoCalling/src/screens/Room.jsx
@@ -1,13 +1,25 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState} from 'react'
 import { useSocket } from '../context/SocketProvider'
 
 const Room = () => {
 
-    const socket = useSocket()
+    const socket = useSocket();
+    const [remoteSocketId, setRemoteSocketId] = useState(null);
+    const [myStream, setMystream] = useState()
 
     const handleUserJoined = useCallback(({email,id}) => {
         console.log(`Email ${email} joined room`);
+        setRemoteSocketId(id)
     },[]);
+
+    const handleCallUser = useCallback(() => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        });
+
+        setMystream(stream)
+    }, []);
 
     useEffect(() => {
         socket.on('user:joined', handleUserJoined);
@@ -19,6 +31,10 @@ const Room = () => {
   return (
     <div>
         <h1>Room Page</h1>
+        <h4>{remoteSocketId ? 'Connected' : 'No one in room'}</h4>
+        {
+            remoteSocketId && <button onClick={handleCallUser}>CALL</button>
+        }
     </div>
   )
 }
