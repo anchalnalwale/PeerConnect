e86321537093a470da77b7d9d5f2ea055d23adc9
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-23 23:57:15 +0530

    Added tailwind

--- client/videoCalling/src/App.jsx
+++ client/videoCalling/src/App.jsx
@@ -3,8 +3,8 @@ import { Routes } from 'react-router'
 import { Route } from 'react-router'
 import { useState } from 'react'
 import Lobby from './screens/Lobby'
-import './App.css'
 import Room from './screens/Room'
+import './App.css'
 
 function App() {
   return (
